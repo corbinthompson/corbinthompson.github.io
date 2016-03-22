2d5911a0463582164514251829044663cdf51b79
Daniel Walnut <danywalnut@gmail.com>
2016-03-22 06:56:25 -0700

    Changed JS resources files into JSONindexes

--- resources.js
+++
@@ -1,7 +0,0 @@
-function LoadResources()
-{
-	TheHeadLines.push(new HeadLine("polaroid_thumb/thumb-I.jpg", "\"I'd walk a thousand miles to see your morphine smile\"", "polaroid_thumb/I.jpg", 0));
-	TheHeadLines.push(new HeadLine("polaroid_thumb/thumb-II.jpg", "&nbsp \"&nbspThe way your hair smells is like a drug\"", "polaroid_thumb/II.jpg", 0));
-	TheHeadLines.push(new HeadLine("polaroid_thumb/thumb-III.jpg", "\"I miss everyone I've seen when you're not around\"", "polaroid_thumb/III.jpg", 0));
-	TheHeadLines.push(new HeadLine("polaroid_thumb/thumb-IV.jpg", "\"I miss everyone I've seen when you're not around\"", "polaroid_thumb/IV.jpg", 0));
-}
